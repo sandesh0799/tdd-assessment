6eb180fd1b75fb3a6fe59af75e06d6586bf21041
sandesh <sandesh0799@gmail.com>
2025-04-27 13:23:46 +0530

    1.Added calculate sum method. 2.Added some css for error message ,result.

--- string-calculator-tdd/src/app/components/calculator/calculator.component.ts
+++ string-calculator-tdd/src/app/components/calculator/calculator.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CalculatorService } from '../../services/calculator.service';
 
 @Component({
   selector: 'app-calculator',
@@ -10,5 +11,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  inputString = '';
+  result: number | null = null;
+  errorMessage = '';
+
+  constructor(private calculatorService: CalculatorService) { }
+
+  calculateSum() {
+    try {
+      this.result = this.calculatorService.add(this.inputString);
+      this.errorMessage = '';
+    } catch (error) {
+      this.result = null;
+      this.errorMessage = error instanceof Error ? error.message : 'Unknown error';
+    }
+  }
 
 }
