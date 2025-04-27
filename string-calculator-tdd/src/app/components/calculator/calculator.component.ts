765578af408bdebbfa823f2dbd469e043a05cd77
sandesh <sandesh0799@gmail.com>
2025-04-27 13:13:32 +0530

    Added CalculatorService

--- string-calculator-tdd/src/app/components/calculator/calculator.component.ts
+++ string-calculator-tdd/src/app/components/calculator/calculator.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
--- string-calculator-tdd/src/app/services/calculator.service.ts
+++ string-calculator-tdd/src/app/services/calculator.service.ts
@@ -6,4 +6,31 @@ import { Injectable } from '@angular/core';
 export class CalculatorService {
 
   constructor() { }
+
+  add(numbers: string): number {
+    if (numbers === '') {
+      return 0;
+    }
+
+    let delimiter = ',';
+    let numbersToProcess = numbers;
+
+    if (numbers.startsWith('//')) {
+      delimiter = numbers.charAt(2);
+      numbersToProcess = numbers.substring(numbers.indexOf('\n') + 1);
+    }
+
+    const normalizedInput = numbersToProcess.replace(/\n/g, delimiter);
+
+    const nums = normalizedInput.split(delimiter).map(num => {
+      return num ? parseInt(num, 10) : 0;
+    });
+
+    const negatives = nums.filter(num => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negatives.join(',')}`);
+    }
+
+    return nums.reduce((sum, num) => sum + num, 0);
+  }
 }
